0fadd2c7e4fe2a622f37518127df90063c8d077d
xyamlx <82068811+xyamlx@users.noreply.github.com>
2021-10-25 16:00:15 +0700

    Modul3

---
+++ src/com/amel/pbo/modul3/percobaan/Rumah.java
@@ -0,0 +1,12 @@
+package com.amel.pbo.modul3.percobaan;
+
+public class Rumah {
+
+    // attribute
+    public String alamatRumah;
+
+    // method
+    public void setAlamat(String x) {
+        alamatRumah = x;
+    }
+}
